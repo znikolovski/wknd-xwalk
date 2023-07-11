d8b0f1758a28cd8ef6248ccaddaceda779062588
Burkhard Pauli <mail@bpauli.de>
2023-07-11 17:13:55 +0200

    fix: missing UE properties

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,7 +2,14 @@ export default async function decorate(block) {
   const row = block.firstElementChild;
   const bg = row.querySelector('picture');
   const bgP = bg.closest('p');
+  cloneAttributes(bg, bgP);
   block.append(bg);
-  if (bgP) bgP.remove();
+  bgP.remove();
   row.classList.add('hero-body');
+}
+
+function cloneAttributes(target, source) {
+  [...source.attributes].forEach( attr => { 
+    if (attr.nodeName !== 'itemtype') target.setAttribute(attr.nodeName ,attr.nodeValue) 
+  })
 }
