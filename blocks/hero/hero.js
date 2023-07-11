9d6f1531b182c6f37f37f16c0f8c306d9c879353
Burkhard Pauli <mail@bpauli.de>
2023-07-11 17:21:06 +0200

    Revert "fix: missing UE properties"
    This reverts commit d8b0f1758a28cd8ef6248ccaddaceda779062588.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,14 +2,7 @@ export default async function decorate(block) {
   const row = block.firstElementChild;
   const bg = row.querySelector('picture');
   const bgP = bg.closest('p');
-  cloneAttributes(bg, bgP);
   block.append(bg);
-  bgP.remove();
+  if (bgP) bgP.remove();
   row.classList.add('hero-body');
-}
-
-function cloneAttributes(target, source) {
-  [...source.attributes].forEach( attr => { 
-    if (attr.nodeName !== 'itemtype') target.setAttribute(attr.nodeName ,attr.nodeValue) 
-  })
 }
